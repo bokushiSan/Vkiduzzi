a5dff436fbbb11b40e1932874bfc9d825b261699
bokushiHuncho <r.sh.nagimov@gmail.com>
2023-02-21 01:33:31 +0500

    Add docstring to files. Rename game_process.py to battle.py. Correct variables in PVP class.

--- utilities/draw_text.py
+++ utilities/draw_text.py
@@ -1,4 +1,12 @@
 def draw_text(text, text_font, text_col, surface, coordinates):
+    """
+    Draw the text.
+    :param text: text to draw
+    :param text_font: font of the text
+    :param text_col: color of the text
+    :param surface: surface to draw
+    :param coordinates: coordinates to draw
+    """
     txt = text_font.render(text, True, text_col)
     text_rect = txt.get_rect(center=coordinates)
     surface.blit(txt, text_rect)
